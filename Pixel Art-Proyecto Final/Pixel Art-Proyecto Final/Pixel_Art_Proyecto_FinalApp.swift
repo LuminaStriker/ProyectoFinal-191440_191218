5545940b4ebeb2af02d1014600727662c28182f2
Roxy Hernandez <76972432+LuminaStriker@users.noreply.github.com>
2025-05-09 09:16:51 -0600

    Add files via upload
    Fase de proyecto inicial, antes de analizar el codigo y convertirlo en uno propio. Sin funcion de capas y renderizado

---
+++ Pixel Art-Proyecto Final/Pixel Art-Proyecto Final/Pixel_Art_Proyecto_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_Art_Proyecto_FinalApp.swift
+//  Pixel Art-Proyecto Final
+//
+//  Created by alumno on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_Art_Proyecto_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
